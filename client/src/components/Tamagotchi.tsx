d5b539b23951c1b4bdc3146e6e36f7b20d6f5b28
robertcprice <73550613+robertcprice@users.noreply.github.com>
2025-09-06 19:37:22 -0400

    fix: ensure tradagotchi animation initializes correctly

--- client/src/components/Tamagotchi.tsx
+++ client/src/components/Tamagotchi.tsx
@@ -17,24 +17,34 @@ export default function Tamagotchi() {
   // Setup character and state machine
   useEffect(() => {
     console.log('🎈 Tamagotchi: Initializing component...');
-    characterRef.current = new TradingCharacter();
-    actorRef.current = createActor(tradingCharacterMachine).start();
-    
-    console.log('🎈 Tamagotchi: Initial state:', actorRef.current.getSnapshot().value);
-    
-    if (containerRef.current) {
-      characterRef.current.initialize(containerRef.current);
+
+    const character = new TradingCharacter();
+    const actor = createActor(tradingCharacterMachine).start();
+
+    characterRef.current = character;
+    actorRef.current = actor;
+
+    console.log('🎈 Tamagotchi: Initial state:', actor.getSnapshot().value);
+
+    let sub: { unsubscribe: () => void } | undefined;
+
+    async function init() {
+      if (containerRef.current) {
+        await character.initialize(containerRef.current);
+      }
+
+      sub = actor.subscribe((state) => {
+        console.log('🎈 Tamagotchi: State changed to:', state.value, 'Context:', state.context);
+        character.play(state.value as string);
+      });
     }
 
-    const sub = actorRef.current.subscribe((state) => {
-      console.log('🎈 Tamagotchi: State changed to:', state.value, 'Context:', state.context);
-      characterRef.current?.play(state.value as string);
-    });
+    init();
 
     return () => {
       console.log('🎈 Tamagotchi: Cleaning up...');
-      sub.unsubscribe();
-      actorRef.current?.stop();
+      sub?.unsubscribe();
+      actor.stop();
     };
   }, []);
 
